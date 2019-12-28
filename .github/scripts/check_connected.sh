4eddadcee6ee2b7fc5239dbc485bd77471d44f98
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2019-12-28 14:12:16 +0100

    Update check_connected.sh

--- .github/scripts/check_connected.sh
+++ .github/scripts/check_connected.sh
@@ -6,6 +6,8 @@ port=`echo $file | grep server-port`
 # if file exists and has a size greater than zero
 if [ -s .players ]; then
   lsof -iTCP:$port -sTCP:ESTABLISHED > .players
+  exit 0
 else
-  lsof -iTCP:$port -sTCP:ESTABLISHED > .players && echo "connected"
+  lsof -iTCP:$port -sTCP:ESTABLISHED > .players && echo "Players Connected"
+  exit 1
 fi
