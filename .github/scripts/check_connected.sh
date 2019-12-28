e65b15d36f359c9cc6913c6bd5ac748bbb4c2594
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2019-12-28 14:53:38 +0100

    Update check_connected.sh

--- .github/scripts/check_connected.sh
+++ .github/scripts/check_connected.sh
@@ -2,9 +2,10 @@
 
 folder=${1:-''}
 dir="/home/minecraft/deployed/$folder"
-file='server.properties'
+file="$dir/server.properties"
 port=`sudo cat $file | grep server-port | awk -F'=' '{print $2}'`
 
+echo $file
 echo $port
 
 # if file exists and has a size greater than zero
