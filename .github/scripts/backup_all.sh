010688cec49762dbd1f8001e6aaaf0ee644bc934
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2019-12-28 18:38:33 +0100

    Update backup_all.sh

--- .github/scripts/backup_all.sh
+++ .github/scripts/backup_all.sh
@@ -6,5 +6,5 @@ folder=${2:-''}
 NOW=$(date +"%m-%d-%Y-%H-%M-%S")
 FILE="backup.$NOW.$hostname.tar.gz"
 
-sudo tar -czf /home/minecraft/backuped/$FILE /home/minecraft/deployed/$folder/world
+sudo tar -czf /home/minecraft/backuped/$FILE /home/minecraft/deployed/$folder/
 echo "done"
